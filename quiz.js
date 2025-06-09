e51ed1f7aa145bec365b95f5cc12f380dbfd0e27
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-09 15:42:58 +0200

    refactor: change operator to strictly equals

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer() {
   const userAnswer = document.querySelector("input[name='quiz']:checked").value;
   let feedback = document.getElementById("feedback");
 
-  if (correctAnswer == userAnswer) {
+  if (userAnswer === correctAnswer) {
     feedback.textContent = "Correct! Well done.";
   } else {
     feedback.textContent = "That's incorrect. Try again!";
