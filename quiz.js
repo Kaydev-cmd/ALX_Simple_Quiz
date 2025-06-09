11c0575195d8725f5b57618ac79e2d558e343dd7
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-09 15:35:04 +0200

    feat: display feedback message based on user's quiz answer

--- quiz.js
+++ quiz.js
@@ -1,3 +1,11 @@
-function checkAnswer () {
+function checkAnswer() {
   const correctAnswer = "4";
+  const userAnswer = document.querySelector("input[name='quiz']:checked").value;
+  let feedback = document.getElementById("feedback");
+
+  if (correctAnswer == userAnswer) {
+    feedback.textContent = "Correct! Well done.";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
 }
